e6b0709538c8fcd67ef819654652fb3515dd9430
Sae14 <marion.baudouin.contact@gmail.com>
2022-05-19 21:58:50 +0200

    configuration de multer pour modifier/supprimer un post avec son image

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -63,38 +63,41 @@ exports.modifyPost = (req, res, next) => {
   })
     .then((post) => {
       if ((req.auth.userId == post.userId) | (req.auth.userRole == "ADMIN")) {
-        // | (req.auth == Post.hasUser.admin(true))
         // S'il y a modification de l'image :
-        // if (req.file) {
-        //   const postObjectFile = {
-        //     ...JSON.parse(req.body.body),
-        //     imageUrl: `${req.protocol}://${req.get("host")}/images/${
-        //       req.file.filename
-        //     }`,
-        //   };
-        //   const filename = post.imageUrl.split("/images/")[1];
-        //   fs.unlink(`images/${filename}`, () => {
-        //     Post.updateOne(
-        //       { id: req.params.id },
-        //       { ...postObjectFile, id: req.params.id }
-        //     )
-        //       .then(() => res.status(200).json({ message: "Post modifié" }))
-        //       .catch((error) => res.status(400).json({ error }));
-        //   });
-        // } else {
-        // S'il y a modification sans image :
-        const postObject = { ...req.body };
-        Post.update(
-          { ...postObject, id: req.params.id },
-          {
-            where: {
-              id: req.params.id,
-            },
-          }
-        )
-          .then(() => res.status(200).json({ message: "Post modifié" }))
-          .catch((error) => res.status(400).json({ error }));
-        // }
+        if (req.file) {
+          const postObjectFile = {
+            // ...JSON.parse(req.body.content),
+            content: req.body.content,
+            image: `${req.protocol}://${req.get("host")}/images/${
+              req.file.filename
+            }`,
+          };
+          const filename = post.image.split("/images/")[1];
+          fs.unlink(`images/${filename}`, () => {
+            Post.update(
+              { ...postObjectFile, id: req.params.id },
+              { where: { id: req.params.id } }
+            )
+              .then(() =>
+                res.status(200).json({ message: "Post modifié avec image" })
+              )
+              .catch((error) => res.status(400).json({ error }));
+          });
+        } else {
+          // S'il y a modification sans image :
+          const postObject = { ...req.body };
+          Post.update(
+            { ...postObject, id: req.params.id },
+            {
+              where: {
+                id: req.params.id,
+              },
+            }
+          )
+            .then(() => res.status(200).json({ message: "Post modifié" }))
+            .catch((error) => res.status(400).json({ error }));
+          // }
+        }
       } else {
         return res.status(403).json({ message: "Unauthorized request" });
       }
@@ -106,12 +109,22 @@ exports.deletePost = (req, res, next) => {
   Post.findOne({ where: { id: req.params.id } })
     .then((post) => {
       if ((req.auth.userId == post.userId) | (req.auth.userRole == "ADMIN")) {
-        // const filename = sauce.imageUrl.split("/images/")[1];
-        // fs.unlink(`images/${filename}`, () => {
-        Post.destroy({ where: { id: req.params.id } })
-          .then(() => res.status(200).json({ message: "Post supprimé" }))
-          .catch((error) => res.status(400).json({ error }));
-        // });
+        if (post.image) {
+          const filename = post.image.split("/images/")[1];
+          fs.unlink(`images/${filename}`, () => {
+            Post.destroy({ where: { id: req.params.id } })
+              .then(() =>
+                res
+                  .status(200)
+                  .json({ message: "Post supprimé avec son image" })
+              )
+              .catch((error) => res.status(400).json({ error }));
+          });
+        } else {
+          Post.destroy({ where: { id: req.params.id } })
+            .then(() => res.status(200).json({ message: "Post supprimé" }))
+            .catch((error) => res.status(400).json({ error }));
+        }
       } else {
         res.status(403).json({ message: "Unauthorized request" });
       }
